d716cff073b5d58b5f655c6efff7431da01f645b
Khaled Abou Hechmeh <6320936@cegepmv.ca>
2026-02-05 08:59:33 -0500

    fixed cpu, disk and ps

--- di_demo/src/disk/disk.module.ts
+++ di_demo/src/disk/disk.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DiskController } from './disk.controller';
 import { DiskService } from './disk.service';
 
 @Module({
-  controllers: [DiskController],
   providers: [DiskService]
 })
 export class DiskModule {}
--- di_demo/src/powersupply/powersupply.module.ts
+++ di_demo/src/powersupply/powersupply.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PowersupplyController } from './powersupply.controller';
 import { PowersupplyService } from './powersupply.service';
 
 @Module({
-  controllers: [PowersupplyController],
   providers: [PowersupplyService]
 })
 export class PowersupplyModule {}
